badacf292603c15e0479a7da6af28d33de6be84c
Florian Kempenich <ShockN745@gmail.com>
2016-04-16 21:09:14 +0200

    Prepare for YABE tutorial

--- src/main/java/com/shockn745/domain/application/driving/impl/GetWhatMessageUseCaseImpl.java
+++ src/main/java/com/shockn745/domain/application/driving/impl/GetWhatMessageUseCaseImpl.java
@@ -4,12 +4,12 @@ import com.shockn745.data.SecretRepository;
 import com.shockn745.domain.application.driving.GetWhatMessageUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Kempenich Florian
  */
-@Component
+@Service
 public class GetWhatMessageUseCaseImpl implements GetWhatMessageUseCase {
 
     private final String defaultString;
---
+++ src/main/java/com/shockn745/presentation/controller/BlogController.java
@@ -0,0 +1,25 @@
+package com.shockn745.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Kempenich Florian
+ */
+@Controller
+@RequestMapping(value = "/yabe")
+public class BlogController {
+
+    @RequestMapping(value = "/main")
+    public String main(Model model) {
+        return showTestMessage(model);
+    }
+
+
+
+    private String showTestMessage(Model model) {
+        model.addAttribute("what", "This is a test");
+        return "what";
+    }
+}
