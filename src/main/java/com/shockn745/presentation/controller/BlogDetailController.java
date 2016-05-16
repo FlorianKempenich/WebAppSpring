7493c18d98dd35ebef9eeff3ced65de669c6e28b
Florian Kempenich <shockn745@gmail.com>
2016-05-16 20:29:08 +0200

    Add tags to detail page

--- src/main/java/com/shockn745/presentation/controller/BlogDetailController.java
+++ src/main/java/com/shockn745/presentation/controller/BlogDetailController.java
@@ -1,6 +1,7 @@
 package com.shockn745.presentation.controller;
 
 import com.shockn745.domain.application.driving.BlogPostDetailUseCase;
+import com.shockn745.domain.application.driving.TagsUseCase;
 import com.shockn745.domain.application.driving.dto.BlogPostDTO;
 import com.shockn745.presentation.util.Disqus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,8 @@ public class BlogDetailController {
 
     @Autowired
     BlogPostDetailUseCase blogPostDetailUseCase;
+    @Autowired
+    TagsUseCase tagsUseCase;
 
     @Autowired
     Disqus disqus;
@@ -33,22 +36,28 @@ public class BlogDetailController {
         return "yabe/post";
     }
 
-    @RequestMapping(value = "/post/{id}")
-    public String postWithId(@PathVariable int id, Model model) {
-        updateModelWithPostInformation(id, model);
-        return "yabe/post";
-    }
-
     private void updateModelWithPostInformation(int postId, Model model) {
         BlogPostDTO post = blogPostDetailUseCase.get(postId);
 
         String title = post.getTitle();
         String htmlContent = blogPostDetailUseCase.getHtml(postId);
 
+        updateModelWithPopularTags(model);
+
         model.addAttribute("title", title);
         model.addAttribute("htmlContent", htmlContent);
 
         model.addAttribute("page_url", disqus.getPageUrl(postId));
         model.addAttribute("page_identifier", disqus.getPageIdentifier(postId));
+    }
+
+    private void updateModelWithPopularTags(Model model) {
+        model.addAttribute("popularTags", tagsUseCase.getPopularTags(8));
+    }
+
+    @RequestMapping(value = "/post/{id}")
+    public String postWithId(@PathVariable int id, Model model) {
+        updateModelWithPostInformation(id, model);
+        return "yabe/post";
     }
 }
