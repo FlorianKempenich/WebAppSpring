dcc669fee67fe8b30dd8cf0ab512161a4c59048c
Florian Kempenich <shockn745@gmail.com>
2016-05-31 17:49:13 +0200

    Clean up and implement popup logic in JS

--- src/main/resources/static/js/yabe/newsletter.js
+++ src/main/resources/static/js/yabe/newsletter.js
@@ -1,49 +1,29 @@
-/**
- * Created by Shock on 5/30/2016.
- */
 
-function cook() {
-    Cookies.set('name', true);
-    // Cookies.remove('name');
-    var cookie = Cookies.get('name');
+function incrementCookieCounter() {
+    var counter = getCookieCounter();
+    counter++;
 
-    if (cookie === 'true') {
-        console.log("true :D :D")
+    Cookies.set('counter', counter);
+}
+
+function removeCookieCounter() {
+    Cookies.remove('counter');
+}
+
+function getCookieCounter() {
+    var counterCookie = Cookies.get('counter');
+    var counter;
+    if (counterCookie == null) {
+        counter = 0;
+    } else {
+        counter = parseInt(counterCookie);
     }
-
+    return counter;
+}
+function displayCookieCounter() {
+    var cookie = getCookieCounter();
     console.log(cookie);
 }
-
-
-
-
-
-
-function setCookie() {
-    Cookies.set('test', 'hello'); //expires in days
-}
-
-function removeCookie() {
-    Cookies.remove('test');
-    return true;
-}
-
-function displayCookie() {
-    var cookie = Cookies.get('test');
-    $('#cookieResult').append(cookie);
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function showModal() {
@@ -51,88 +31,30 @@ function showModal() {
     console.log("opening");
 }
 
-function automaticPopup(mode, autoClose) {
+/**
+ * Display the newsletter popup after the user scrolled to the bottom of 3 pages.
+ * Do not display for 2 months after that.
+ *
+ * todo: If user enter email, never display again
+ */
+function newsletterPopupInit() {
 
-    $('#newsletterPopup').on('hidden.bs.modal', function (e) {
-        setCookie();
-    });
+    // todo: If user enter email, never display again
+    // $('#newsletterPopup').on('hidden.bs.modal', function (e) {
+    //     setCookie();
+    // });
 
-
-    switch(mode) {
-        case "atendpage":
-            $(window).scroll(function(){
-                var yPos = $(window).scrollTop();
-                if (yPos >= ($(document).height() - $(window).height()) ) {
-                    showModal();
-                } else {
-                    if (yPos + 300 < ($(document).height() - $(window).height()) ) {
-                        if(autoClose == true) {
-                            closeWindow();
-                        }
-                    }
-                }
-
-            });
-            break;
-        
-        case "onload":
-            $(window).load(function(){
+    $(window).scroll(function () {
+        var yPos = $(window).scrollTop();
+        if (yPos >= ($(document).height() - $(window).height())) {
+            var counter = getCookieCounter();
+            if (counter === 2) {
                 showModal();
-                if(autoClose == true) {
-                    scrollDetection("scrollStart", function() {
-                        closeWindow();
-                    });
-
-                }
-            });
-            break;
-
-        case "onidle":
-
-            $(window).load(function(){
-                scrollDetection("scrollEnd", function() {
-                    showModal();
-                });
-
-                if(autoClose == true) {
-                    scrollDetection("scrollStart", function() {
-                        closeWindow();
-                    });
-                }
-            });
-
-            break;
-    }
-
-}
-
-function scrollDetection(trigger, onDone) {
-    var t, l = (new Date()).getTime();
-
-    $(window).scroll(function(){
-        var now = (new Date()).getTime();
-
-        if(now - l > 400){
-            $(this).trigger('scrollStart');
-            l = now;
+            }
+            if (counter <= 2) {
+                incrementCookieCounter();
+            }
         }
-
-        clearTimeout(t);
-        t = setTimeout(function(){
-            $(window).trigger('scrollEnd');
-        }, 300);
     });
-    if (trigger == "scrollStart") {
-        $(window).bind('scrollStart', function(){
-            $(window).unbind('scrollEnd');
-            onDone();
-        });
-    }
 
-    if (trigger == "scrollEnd") {
-        $(window).bind('scrollEnd', function(){
-            $(window).unbind('scrollStart');
-            onDone();
-        });
-    }
 }
