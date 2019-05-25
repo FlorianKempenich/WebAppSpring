292832f9244158133958d591e868fb6488eecaf0
Florian Kempenich <Flori@nKempenich.com>
2019-05-25 13:56:38 +0100

    Add note

--- build_and_start.sh
+++ build_and_start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Update blog posts location at 'src/main/java/com/shockn745/DataConfiguration.java'"
+echo "Update blog posts location at 'src/main/java/com/shockn745/DataConfiguration.java'"
+echo "Update blog posts location at 'src/main/java/com/shockn745/DataConfiguration.java'"
+echo "Update blog posts location at 'src/main/java/com/shockn745/DataConfiguration.java'"
+echo "Update blog posts location at 'src/main/java/com/shockn745/DataConfiguration.java'"
+echo ""
 
 ./gradlew build -x test 
 java -jar ./build/libs/webapp-0.0.1-SNAPSHOT.jar
